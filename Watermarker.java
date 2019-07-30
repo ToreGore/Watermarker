8bd09d345ac0389c48db3218df7f6ce1c0a8906c
ToreGore <ettore.t.galante@gmail.com>
2019-07-30 15:59:49 +0200

    First commit -> Created Watermark class. Theoretically capable of watermarking one or more images

---
+++ Watermarker.java
@@ -0,0 +1,66 @@
+import java.awt.AlphaComposite;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Watermarker {
+
+    BufferedImage clean, watermark;
+    ArrayList<BufferedImage> toBeMarked;
+
+    public Watermarker() {
+        this.toBeMarked = new ArrayList<>();
+    }
+
+    public Watermarker(BufferedImage clean, BufferedImage watermark) {
+        this.toBeMarked = new ArrayList<>();
+        this.clean = clean;
+        this.watermark = watermark;
+    }
+
+    public Watermarker(ArrayList<BufferedImage> toBeMarked, BufferedImage watermark) {
+        this.toBeMarked = toBeMarked;
+        this.watermark = watermark;
+    }
+
+    public void markSingleImage(File destination) {
+        try {
+            Graphics2D g2d = (Graphics2D) clean.getGraphics();
+            AlphaComposite alphaChannel = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.3f);
+            g2d.setComposite(alphaChannel);
+            //Calculate image's coordinates
+            int topLeftX = (clean.getWidth() - watermark.getWidth()) / 2;
+            int topLeftY = (clean.getHeight() - watermark.getHeight()) / 2;
+            //Paints the image
+            g2d.drawImage(watermark, topLeftX, topLeftY, null);
+            ImageIO.write(clean, "jpg", destination);
+            g2d.dispose();
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void markBatch(File destination) {
+        for (BufferedImage img : toBeMarked) {
+            markSingleImage(destination);
+        }
+    }
+
+    public void setClean(BufferedImage clean) {
+        this.clean = clean;
+    }
+
+    public BufferedImage getClean() {
+        return clean;
+    }
+
+    public void setWatermark(BufferedImage watermark) {
+        this.watermark = watermark;
+    }
+
+    public BufferedImage getWatermark() {
+        return watermark;
+    }
+
+}
