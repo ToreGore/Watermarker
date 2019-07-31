0af12516f9caafb3f7809e86501e05280def97e1
ToreGore <ettore.t.galante@gmail.com>
2019-07-31 14:24:34 +0200

    Tenth commit -> Added shell interaction

--- Main.java
+++ Main.java
@@ -8,12 +8,71 @@ import java.util.ArrayList;
 import javax.imageio.ImageIO;
 
 public class Main {
-    public static void main(String[] args) {
-        String clean = new String(args[0]);
-        String watermark = new String(args[1]);
-        Watermarker watermarker = new Watermarker(clean, watermark);
+    public static void main(String[] args) throws IOException {
+        
+        String answer = new String();
+        String image = new String();
+        String watermark = new String();
+        String newName = new String();
+        System.out.println("Watermarking unit active");
+        do {
+            System.out.println("\tWhich action would you like to exectute?");
+            System.out.println("\n>\t+\t->\tWatermark an image");
+            System.out.println("\n>\tBatch\t->\tWatermark more images together");
+            System.out.println("\n>\tCredits\t->\tAbout me");
+            System.out.println("\n>\tStop\t->\tExit program");
+            System.out.print("Operation:\t");
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            answer = reader.readLine();
+            reader.close();
+            switch(answer) {
+                case "+":
+                    System.out.print("Path and name of watermarkable image:\t");
+                    reader = new BufferedReader(new InputStreamReader(System.in));
+                    image = reader.readLine();
+                    reader.close();
+                    System.out.print("Path and name of watermark:\t");
+                    reader = new BufferedReader(new InputStreamReader(System.in));
+                    watermark = reader.readLine();
+                    reader.close();
+                    System.out.print("Name of new image:\t");
+                    reader = new BufferedReader(new InputStreamReader(System.in));
+                    newName = reader.readLine();
+                    reader.close();
+                    Watermarker watermarker = new Watermarker(image, watermark);
+                    watermarker.markSingleImage(new File("./" + newName));
+                    System.out.println("**Execution successful!**");
+                break;
+                case "Batch":
+                    workInProgress();
+                break;
+                case "Credits":
+                    System.out.println(aboutMe());
+                break;
+                case "Stop":
+                    System.out.print("Thank you for your patronage! Have a nice day!");
+                break;
+                default:
+                    System.out.println("Operation denied. Retry!");
+                break;
+            }
+
+        } while(!answer.equals("Stop"));
+
+        
         //watermarker.markSingleImage(new File("./Desktop_" + clean.substring(clean.lastIndexOf('/') + 1)));
-	watermarker.markSingleImage(new File("./Desktop_" + args[2]));
           
     }
+
+    public static String aboutMe() {
+        String aboutMe = new String("\tGithub Name:\tToreGore\n\tGithub Link:\thttps://github.com/ToreGore\n\tOccupation:\tCurrently enrolled in the Master of Computer Science at the University of Studies of Milan.");
+        return aboutMe;
+    }
+    public static void workInProgress() {
+        System.out.println("***************************************");
+        System.out.println("** Functionality not yet implemented **");
+        System.out.println("***************************************");
+    }
+
 }
